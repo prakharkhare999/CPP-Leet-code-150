4ebc0727726035977c0f0b124bb76883b0d49370
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-02-27 22:24:54 +0530

    Create Pattern_5.cpp

---
+++ Pattern/Pattern_5.cpp
@@ -0,0 +1,25 @@
+1
+12
+123
+1234
+12345
+
+//cpp
+#include <iostream>
+using namespace std;
+
+
+int main() {
+ int n;
+cin>>n;
+
+
+ 
+  for(int i=0;i<n;i++){
+    for(int j=0;j<i+1;j++){
+      cout<<j+1;
+    }
+    cout<<endl;
+  }
+}
+
